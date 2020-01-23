2c74714ddafd47c7398a255ad660118fd80e3480
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-23 16:24:53 +0100

    Versione-php conclusa, bisogna fare la versione-ajax

---
+++ versione-php/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <link rel="stylesheet" href="public/css/app.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>Versione Include PHP</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <img src="public/images/logo.png" alt="logo" />
+            </div>
+        </header>
+        <main>
+            <!-- <select id="selector">
+                <option value="">Scegliere il genere</option>
+                <option value="pop">Pop</option>
+                <option value="rock">Rock</option>
+                <option value="metal">Metal</option>
+                <option value="jazz">Jazz</option>
+            </select> -->
+            <div class="cds-container container">
+                <?php
+
+                include 'data.php';
+
+                foreach ($data as $album) { ?>
+                    <div class="cd" data-genre="<?php echo $album['genre'] ?>">
+                        <img src="<?php echo $album['poster']; ?>" alt="">
+                        <h3><?php echo $album['title']?></h3>
+                        <span class="author"><?php echo $album['author'] ?></span>
+                        <span class="year"><?php echo $album['year']?></span>
+                    </div>
+                <?php
+                } ?>
+            </div>
+        </main>
+    </body>
+</html>
