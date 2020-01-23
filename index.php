c7341d71d7697fb5acafd58a43b51e5234caf76b
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-23 16:09:05 +0100

    Lo stile è sistemato con Sass, ora sotto con PHP

--- index.php
+++ index.php
@@ -21,10 +21,24 @@ include 'data.php'
         <title></title>
     </head>
     <body>
-        <?php echo "Hello World" ?>
-        <?php echo "<pre>" ?>
-        <?php var_dump($data) ?>
-        <?php echo "<pre>" ?>
+        <header>
+            <div class="container">
+                <img src="logo.png" alt="logo" />
+            </div>
+        </header>
+        <main>
+            <!-- <select id="selector">
+                <option value="">Scegliere il genere</option>
+                <option value="pop">Pop</option>
+                <option value="rock">Rock</option>
+                <option value="metal">Metal</option>
+                <option value="jazz">Jazz</option>
+            </select> -->
+            <div class="cds-container container">
+                <?php echo "Hello World" ?>
+                <?php var_dump($data) ?>
+            </div>
+        </main>
         <script id="template" type="text/x-handlebars-template">
             <div class="cd" data-genre="{{ genre }}">
                 <img src="{{ cover }}" alt="{{author}} - {{title}}">
